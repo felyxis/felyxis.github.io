766bdda153601d501d2f52d8e72e706040829980
felyxis <ivancencofelicia@gmail.com>
2017-11-13 19:41:18 +0200

    device motion

--- js/senzori.js
+++ js/senzori.js
@@ -1,6 +1,7 @@
 
 window.addEventListener("deviceorientation", on_device_orientation );
-
+window.addEventListener("devicemotion", on_device_motion );
+//----------------------
 function on_device_orientation(e)
 {
 
@@ -8,4 +9,10 @@ document.getElementById("id_z").innerHTML = "z="+ e.alpha ;
 document.getElementById("id_x").innerHTML = "x="+ e.beta ;
 document.getElementById("id_y").innerHTML =	"y="+ e.gamma ;
 
+}
+//------------------------
+function on_device_motion(e)
+{
+	
+	
 }

6fdfe40bbc88f5a0e25b9e2da94e5550d165c9eb
felyxis <ivancencofelicia@gmail.com>
2017-11-20 19:16:48 +0200

    sensor

--- js/senzori.js
+++ js/senzori.js
@@ -17,7 +17,7 @@ function on_device_motion(e)
 	document.getElementById("id_acc_g_x").innerHTML="acc g x =" + Math.round(e.accelerationIncludingGravity.x * 100)/100;
 	document.getElementById("id_acc_g_y").innerHTML="acc g y =" + Math.round(e.accelerationIncludingGravity.y * 100)/100;
 	document.getElementById("id_acc_g_z").innerHTML="acc g z =" + Math.round(e.accelerationIncludingGravity.z * 100)/100;
-	var -beta = Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
+	var beta = -Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
 	var gamma = Math.atan(e.accelerationIncludingGravity.y/e.accelerationIncludingGravity.z)*180/Math.PI;
 	document.getElementById("id_rot_x").innerHTML="rot x =" + Math.round(beta* 100)/100;
 	document.getElementById("id_rot_y").innerHTML="rot y =" + Math.round(gamma* 100)/100;

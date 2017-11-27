80e54eb3f188c657ea00cb6f2c55165aeb3ff483
felyxis <ivancencofelicia@gmail.com>
2017-11-27 19:43:01 +0200

    senzori canvas

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -1,5 +1,6 @@
- document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.1";
- window.addEventListener("deviceorientation",on_device_orientation);
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.7";
+ //window.addEventListener("deviceorientation",on_device_orientation);// sa nu se deseneze de 2 ori cercul
+ window.addEventListener("devicemotion",on_device_motion);
 
  //---------------------------------------------------------------
  function deseneaza_cerc(unghi1,unghi2)
@@ -7,17 +8,31 @@
 	var canvas = document.getElementById("id_canvas");
 	var ctx = canvas.getContext("2d");
 	ctx.clearRect(0,0,400,400);
+    ctx.rect(0,0,400,400);
+	ctx.stroke();
 	ctx.beginPath();
 	ctx.arc(200 + unghi1*200/90,200 + unghi2*200/90,20,0,2*Math.PI,true);
 	ctx.fillStyle="#FF0000";//se umple cercul cu culoarea este rosu
-	ctx.fill();
+	//ctx.fill();
 	ctx.strokeStyle="#00FF00";
 	ctx.lineWidth=5;
+
 	ctx.stroke(); 
  }
- 
+ //----------------------------------------------------------------
+ function on_device_motion(e)
+{
+	var beta = - Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
+	var gamma = Math.atan(e.accelerationIncludingGravity.y/e.accelerationIncludingGravity.z)*180/Math.PI;
+	
+	deseneaza_cerc(beta,gamma);
+	
+}
  //--------------------------------------------
  function on_device_orientation(e)
 {
 	deseneaza_cerc(e.gamma,e.beta);
 }
+
+
+//

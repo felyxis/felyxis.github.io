e92c0b64c1734b14a60289670f5d968d3cc7d358
felyxis <ivancencofelicia@gmail.com>
2017-11-27 18:37:15 +0200

    canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,9 +1,9 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.1";
 
 var canvas = document.getElementById("id_canvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-//ctx.moveTo(50, 50);
+ctx.moveTo(0, 0);
 ctx.lineTo(100, 100);
 ctx.stroke();

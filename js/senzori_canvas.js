826322e9c9ea952cecbba60c4aec1183ddaca324
felyxis <ivancencofelicia@gmail.com>
2017-11-27 19:19:27 +0200

    senzori_canvas

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.0";
+ document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.1";
  window.addEventListener("deviceorientation",on_device_orientation);
 
  //---------------------------------------------------------------
@@ -6,7 +6,7 @@ document.getElementById("id_business_version").innerHTML = "Business version = 2
  {
 	var canvas = document.getElementById("id_canvas");
 	var ctx = canvas.getContext("2d");
-
+	ctx.clearRect(0,0,400,400);
 	ctx.beginPath();
 	ctx.arc(200 + unghi1*200/90,200 + unghi2*200/90,20,0,2*Math.PI,true);
 	ctx.fillStyle="#FF0000";//se umple cercul cu culoarea este rosu
@@ -19,5 +19,5 @@ document.getElementById("id_business_version").innerHTML = "Business version = 2
  //--------------------------------------------
  function on_device_orientation(e)
 {
-	deseneaza_cerc(e.beta,e.gamma);
+	deseneaza_cerc(e.gamma,e.beta);
 }

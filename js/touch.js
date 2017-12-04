f545fde2e2a5707bc48e561f95033203401a587a
felyxis <ivancencofelicia@gmail.com>
2017-12-04 19:38:07 +0200

    touch

--- js/touch.js
+++ js/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.1";
 
 var canvas= document.getElementById("id_canvas");
 var context=canvas.getContext("2d");

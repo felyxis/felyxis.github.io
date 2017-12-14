71ee9d4e696491201878eb3e98755d6468a2e706
felyxis <ivancencofelicia@gmail.com>
2017-12-14 18:40:03 +0200

    touch move

--- js/touch.js
+++ js/touch.js
@@ -1,11 +1,11 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.3";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.4";
 
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 var lista_culori=["#FF0000","#00FF00","#0000FF","#FFFF00","#FF00FF"];
 canvas.addEventListener("touchstart",on_start_touch);
-canvas.addEventListener("touchstart",on_move_touch);
+canvas.addEventListener("touchmove",on_move_touch);
 //---------------------------------------
 function on_start_touch(e)
 {

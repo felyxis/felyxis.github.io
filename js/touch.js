8d13837354d5855ba442a5010c2e8e2d4ca13833
felyxis <ivancencofelicia@gmail.com>
2017-12-14 18:44:57 +0200

    touch move

--- js/touch.js
+++ js/touch.js
@@ -1,36 +1,32 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.4";
-
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1"
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
-var lista_culori=["#FF0000","#00FF00","#0000FF","#FFFF00","#FF00FF"];
-canvas.addEventListener("touchstart",on_start_touch);
-canvas.addEventListener("touchmove",on_move_touch);
+
+canvas.addEventListener("touchstart", on_start_touch);
+canvas.addEventListener("touchmove", on_move_touch);
+
+
+var lista_culori = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#FFFFFF"];
 //---------------------------------------
 function on_start_touch(e)
 {
-var atingere = e.touches;
-for(var i=0; i<atingere.length; i++)
-{
-	context.beginPath();
-	context.arc(atingere.item(i).pageX,atingere.item(i).pageY,20,0,2*Math.PI);
-	context.strokeStyle=lista_culori[i];
-	context.lineWidth=5;
-	context.stroke(); 
-}
-	
+	var touches = e.touches; 
+	for (var i = 0; i < touches.length; i++){
+		context.beginPath();
+		context.arc(touches.item(i).pageX, touches.item(i).pageY, 20, 0, 2 * Math.PI);
+		context.strokeStyle = lista_culori[i];
+		context.stroke();
+	}
 }
 //---------------------------------------
 function on_move_touch(e)
 {
-var atingere = e.touches;
-for(var i=0; i<atingere.length; i++)
-{
-	context.beginPath();
-	context.arc(atingere.item(i).pageX,atingere.item(i).pageY,20,0,2*Math.PI);
-	context.strokeStyle=lista_culori[i];
-	context.lineWidth=5;
-	context.stroke(); 
-}
-	
+	var touches = e.touches; 
+	for (var i = 0; i < touches.length; i++){
+		context.beginPath();
+		context.arc(touches.item(i).pageX, touches.item(i).pageY, 20, 0, 2 * Math.PI);
+		context.strokeStyle = lista_culori[i];
+		context.stroke();
+	}
 }

0573c95f93a91683eae4f07fae701392ceb67bc4
felyxis <ivancencofelicia@gmail.com>
2017-12-14 18:47:30 +0200

    touch file

--- js/touch.js
+++ js/touch.js
@@ -1,32 +1,41 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1"
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.6";
+
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
-
-canvas.addEventListener("touchstart", on_start_touch);
-canvas.addEventListener("touchmove", on_move_touch);
-
-
-var lista_culori = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#FFFFFF"];
+var lista_culori=["#FF0000","#00FF00","#0000FF","#FFFF00","#FF00FF"];
+canvas.addEventListener("touchstart",on_start_touch);
+canvas.addEventListener("touchmove",on_move_touch);
 //---------------------------------------
 function on_start_touch(e)
 {
-	var touches = e.touches; 
-	for (var i = 0; i < touches.length; i++){
-		context.beginPath();
-		context.arc(touches.item(i).pageX, touches.item(i).pageY, 20, 0, 2 * Math.PI);
-		context.strokeStyle = lista_culori[i];
-		context.stroke();
-	}
+var atingere = e.touches;
+for(var i=0; i<atingere.length; i++)
+{
+	context.beginPath();
+	context.arc(atingere.item(i).pageX,atingere.item(i).pageY,20,0,2*Math.PI);
+	context.strokeStyle=lista_culori[i];
+	context.fillStyle=lista_culori[i];
+	context.fill();
+	context.lineWidth=5;
+	context.stroke(); 
+}
+	
 }
 //---------------------------------------
 function on_move_touch(e)
 {
-	var touches = e.touches; 
-	for (var i = 0; i < touches.length; i++){
-		context.beginPath();
-		context.arc(touches.item(i).pageX, touches.item(i).pageY, 20, 0, 2 * Math.PI);
-		context.strokeStyle = lista_culori[i];
-		context.stroke();
-	}
+var atingere = e.touches;
+for(var i=0; i<atingere.length; i++)
+{
+	context.beginPath();
+	context.arc(atingere.item(i).pageX,atingere.item(i).pageY,20,0,2*Math.PI);
+	context.strokeStyle=lista_culori[i];
+	context.fillStyle=lista_culori[i];
+	context.fill();
+	context.lineWidth=5;
+	context.stroke(); 
 }
+	
+}
+//---------------------------------------

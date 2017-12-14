0a2740396ae06055dbdde1a46c2fd6ae6a680c35
felyxis <ivancencofelicia@gmail.com>
2017-12-14 19:22:44 +0200

    accuracy

--- js/gps.js
+++ js/gps.js
@@ -1,13 +1,13 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
 //---------------------------------
 function on_position_success(e)
 {
-	 document.getElementById("id_lat").innerHTML ="Latitude"+ e.coords.latitude;
-	 document.getElementById("id_long").innerHTML ="Longitude"+ e.coords.longitude;
-	 document.getElementById("id_acc").innerHTML ="Accuracy"+ e.coords.accuracy + "m";
+	 document.getElementById("id_lat").innerHTML ="Latitude="+ e.coords.latitude;
+	 document.getElementById("id_long").innerHTML ="Longitude="+ e.coords.longitude;
+	 document.getElementById("id_acc").innerHTML ="Accuracy="+ e.coords.accuracy + "m";
 	
 }
 //-------------------------------

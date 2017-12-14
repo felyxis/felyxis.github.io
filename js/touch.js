69797031725ac1d4c3b1440649d8b9ffc5881c35
felyxis <ivancencofelicia@gmail.com>
2017-12-14 18:22:08 +0200

    touch

--- js/touch.js
+++ js/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.4";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0";
 
 var canvas= document.getElementById("id_canvas");
 var context=canvas.getContext("2d");
@@ -9,9 +9,9 @@ var lista_culori=["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FFFFFF"];
 
 function on_start_touch(e)
 {
-	e.changedTouches;
+	e.Touches;
 	
-	var touches=e.changedTouches;
+	var touches=e.Touches;
 	for (var i=0; i< touches.length; i++) {
 		context.beginPath();
 		context.arc(touches.item(i).pageX, touches.item(i).pageY,20, 0, 2*Math.PI);

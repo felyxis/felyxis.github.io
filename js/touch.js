b3c764f21c149778195aea3f50d92884f5eca0d5
felyxis <ivancencofelicia@gmail.com>
2017-12-14 18:30:37 +0200

    touch

--- js/touch.js
+++ js/touch.js
@@ -1,25 +1,22 @@
 document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1";
 
-var canvas= document.getElementById("id_canvas");
-var context=canvas.getContext("2d");
 
-canvas.addEventListener("touchstart", on_start_touch);
-var lista_culori=["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FFFFFF"];
-//-----------------------
-
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+var lista_culori=["#FF0000","#00FF00","#0000FF","#FFFF00","#FFFFFF"];
+canvas.addEventListener("touchstart",on_start_touch);
+//---------------------------------------
 function on_start_touch(e)
 {
-
-	
-	var touches=e.touches;
-	for (var i=0; i< touches.length; i++) {
-		context.beginPath();
-		context.arc(touches.item(i).pageX, touches.item(i).pageY,20, 0, 2*Math.PI);
-		context.strokeStyle=lista_culori[i];
-		context.stroke();
-		
-		
-	}
+var atingere = e.touches;
+for(var i=0; i<atingere.length; i++)
+{
+	context.beginPath();
+	context.arc(atingere.item(i).pageX,atingere.item(i).pageY,20,0,2*Math.PI);
+	context.strokeStyle=lista_culori[i];
+	context.lineWidth=5;
+	context.stroke(); 
+}
 	
 }
 //-----------------------

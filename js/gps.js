d45763e4f16691f8d92f6de41520153769c0bded
felyxis <ivancencofelicia@gmail.com>
2017-12-14 19:30:45 +0200

    altitude

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.3";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
@@ -8,7 +8,7 @@ function on_position_success(e)
 	 document.getElementById("id_lat").innerHTML ="Latitude="+ e.coords.latitude;
 	 document.getElementById("id_long").innerHTML ="Longitude="+ e.coords.longitude;
 	 document.getElementById("id_acc").innerHTML ="Accuracy="+ e.coords.accuracy + "m";
-	
+	 document.getElementById("id_alt").innerHTML ="Altitude="+ e.coords.altitude + "m";
 }
 //-------------------------------
 function on_position_failure(e)

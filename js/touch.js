47e4ac938084da60d64515b9d85b54bdd3c14ff1
felyxis <ivancencofelicia@gmail.com>
2017-12-14 18:34:45 +0200

    touch

--- js/touch.js
+++ js/touch.js
@@ -1,10 +1,11 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.3";
 
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 var lista_culori=["#FF0000","#00FF00","#0000FF","#FFFF00","#FFFFFF"];
 canvas.addEventListener("touchstart",on_start_touch);
+canvas.addEventListener("touchstart",on_move_touch);
 //---------------------------------------
 function on_start_touch(e)
 {
@@ -20,3 +21,17 @@ for(var i=0; i<atingere.length; i++)
 	
 }
 //-----------------------
+canvas.addEventListener("touchstart",on_move_touch);
+{
+	function on_move_touch(e)
+	var atingere = e.touches;
+for(var i=0; i<atingere.length; i++)
+{
+	context.beginPath();
+	context.arc(atingere.item(i).pageX,atingere.item(i).pageY,20,0,2*Math.PI);
+	context.strokeStyle=lista_culori[i];
+	context.lineWidth=5;
+	context.stroke(); 
+	
+}
+}

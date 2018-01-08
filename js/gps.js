4e6577d8c531bc23cdef520c7b0818548b088433
felyxis <ivancencofelicia@gmail.com>
2018-01-08 18:59:43 +0200

    sala unirii

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.11";
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.12";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
@@ -12,7 +12,7 @@ function on_position_success(e)
 	 
 	 var map_str="https://maps.googleapis.com/maps/api/staticmap?"+
 			"center="+ e.coords.latitude + "," + e.coords.longitude +
-			"&zoom=16" +
+			"&zoom=13" +
 			"&size=400x300" +
 			"&key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
 			"&markers=color:blue|label:A|" + e.coords.latitude + "," + e.coords.longitude +

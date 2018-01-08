1c43ebd2db33971d20c6c9bf1d852ea5a717c601
felyxis <ivancencofelicia@gmail.com>
2018-01-08 17:49:08 +0200

    harta gps

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.1";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 

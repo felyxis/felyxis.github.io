19d4e97f835ac30c8954b205f0e7d96d619e1269
felyxis <ivancencofelicia@gmail.com>
2018-01-08 17:41:37 +0200

    harta gps

--- js/gps.js
+++ js/gps.js
@@ -11,7 +11,7 @@ function on_position_success(e)
 	 document.getElementById("id_alt").innerHTML ="Altitude="+ e.coords.altitude + "m";
 	 
 	 var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+ 
-			e.coords.latitude+","+e.coords.longitude
+			e.coords.latitude+","+e.coords.longitude+
 			"&zoom=15"+
 			"&size=400x300"+
 			"key=AIzaSyBSUQG8KDeE-NcLMXe8dlfQzhv1eh-CDu0";

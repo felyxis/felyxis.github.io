f1713aa79829808bb90137b6d33047063da0ea6d
felyxis <ivancencofelicia@gmail.com>
2018-01-15 19:26:32 +0200

    confidance

--- js/speech.js
+++ js/speech.js
@@ -1,6 +1,6 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
-d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
+d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 
 var speech=new webkitSpeechRecognition();
 speech.onresult= on_speech_results;
@@ -20,6 +20,6 @@ function on_speech_end()
 //----------------------------
 function on_speech_results(e)
 {
-	document.getElementById("id_speech").innerHTML= e.results.item(0).item(0).transcript;
+	document.getElementById("id_speech").innerHTML= e.results.item(0).item(0).transcript +" " + e.results.item(0).item(0).confidance;
 }
 //-----------------------------

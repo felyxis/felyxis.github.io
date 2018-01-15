2416a13c1bff18f23382e2dbd14732192257a7b4
felyxis <ivancencofelicia@gmail.com>
2018-01-15 19:07:12 +0200

    speech

--- js/speech.js
+++ js/speech.js
@@ -1,8 +1,8 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
-d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
 
-var speech=new SpeechRecognition();
+var speech=new webkitSpeechRecognition();
 speech.onresult= on_speech_result;
 speech.onspeechend=on_speech_end;
 speech.lang="en-US";

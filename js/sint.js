fa20d3e96233640c8dfdace12bbb48fe611379e0
felyxis <ivancencofelicia@gmail.com>
2018-01-15 18:21:12 +0200

    synt

--- js/sint.js
+++ js/sint.js
@@ -1,11 +1,11 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".4"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".5"; 
 
 //document.addEventListener("touchstart",speak);
 
 var synth= window.speechSynthesis;
 
-//_____________________________%%%%%_________________
+//-----------------------------
 function on_get_voices()
 {
 	var voci= synth.getVoices();
@@ -23,5 +23,16 @@ function speak()
 	var enunt = new SpeechSynthesisUtterance();
 	enunt.lang = "ru-RU"
 	enunt.text = document.getElementById("id_text").value;
+	enunt.onend = on_end_synth;
+	document.getElementById("id_speak_button").disabled=true;
 	synth.speak(enunt);
 }
+//-----------------
+
+
+function on_end_synth()
+{
+	
+}
+
+//-----------

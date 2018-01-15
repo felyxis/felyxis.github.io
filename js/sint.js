b548c574e2897794bfe1f29ed9469c29ad91b6c6
felyxis <ivancencofelicia@gmail.com>
2018-01-15 18:08:31 +0200

    ...

--- js/sint.js
+++ js/sint.js
@@ -21,7 +21,7 @@ function on_get_voices()
 function speak()
 {
 	var enunt = new SpeechSynthesisUtterance();
-	enunt.lang = "en-US"
+	enunt.lang = "ru-RU"
 	enunt.text = document.getElementById("id_text").value;
 	synth.speak(enunt);
 }

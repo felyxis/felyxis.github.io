8dc9753f633e0127dd588a710e2e22a0b6f06953
felyxis <ivancencofelicia@gmail.com>
2018-01-15 19:43:44 +0200

    speech

--- js/speech.js
+++ js/speech.js
@@ -1,30 +1,30 @@
-var d = new Date();
+var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
-d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".5"; 
+d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".6";
 
-var speech=new webkitSpeechRecognition();
-speech.onresult= on_speech_results;
-speech.onspeechend=on_speech_end;
-speech.lang="en-US";
-speech.maxAlternatives=5;
+//...........................................
+var speech = new webkitSpeechRecognition();
+speech.onresult = on_speech_results;
+speech.onspeechend = on_speech_end;
+speech.lang = "en-US";
+speech.maxAlternatives = 5;
 
-//-------------------------
+//...................................
 function recognize()
 {
 	speech.start();
 }
-//-------------------------
+//............................
 function on_speech_end()
 {
 	speech.stop();
 }
-//----------------------------
+//..............................
 function on_speech_results(e)
-{
-	for(var i=0; i< speech.maxAlternatives; i++)
-	{
-		document.getElementById("id_speech").innerHTML+=
-	 e.results.[0][i].transcript +" " + e.results.[0][i].confidence + "<br>";
+{	
+	for (var i = 0; i < speech.maxAlternatives; i++)
+	document.getElementById ("id_speech").innerHTML +=
+		e.results[0][i].transcript + " " + e.results[0][i].confidence + "<br>";
+	
 }
-}
-//-----------------------------
+//................................

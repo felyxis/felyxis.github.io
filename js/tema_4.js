bf0442d5d68997a3dacab8c2d9cb8a87705e3436
felyxis <ivancencofelicia@gmail.com>
2018-01-24 17:41:30 +0200

    00

--- js/tema_4.js
+++ js/tema_4.js
@@ -23,3 +23,39 @@ function on_end_synth()
 {
 	
 }
+//-==================locatia utilizatorului----------
+function on_position_success(e) 
+{ 
+	lat = e.coords.latitude;
+	lon = e.coords.longitude;
+
+	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=600x500"+
+		"&key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
+			"&markers=color:blue|label:F|"+
+				e.coords.latitude  + "," + e.coords.longitude;
+	document.getElementById("id_img").src=map_str;
+	document.getElementById("id_explicatie").innerHTML = "Afisarea locatiei";
+	
+}
+//------------------
+function line(e)
+{
+		var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  lat + "," + lon + "&zoom=15"+"&size=600x500"+
+		"key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
+			"&markers=color:blue|label:Z|"+ lat  + "," + lon + 
+	  "&path=color:red|" + lat + "," + lon + "|" + document.getElementById("id_speech").innerHTML;
+	document.getElementById("id_img").src=map_str;
+	document.getElementById("id_explicatie").innerHTML = "Trasarea liniei de conectare intre pozitia curentare si cea recunoscuta";
+		
+	
+}
+
+function on_position_failure(e)
+{
+	
+	alert("I'm lost!");
+	
+}
+//------------

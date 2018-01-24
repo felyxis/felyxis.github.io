7c176bba85e16656c24ec55d907caaa15de7c2f9
felyxis <ivancencofelicia@gmail.com>
2018-01-24 18:18:17 +0200

    000

--- js/tema_4.js
+++ js/tema_4.js
@@ -1,28 +1,35 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." 
-+ (d.getMonth()+1) + "." + d.getDate() + ".1"; 
++ (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
-var synth= window.speechSynthesis;
+var enunt = new SpeechSynthesisUtterance();
 var lat ;
-var lon ;  
+var lon ; 
+ enunt.onresult = on_enunt_results;
+ enunt.onend = on_enunt_end;
+ enunt.lang="en-US";
+ //------------
 
-
-function speak()
+ function speak()
 {
-	var enunt = new SpeechSynthesisUtterance();
-	enunt.lang = "en-US"
-	enunt.text = document.getElementById("id_text").value;
-	enunt.onend = on_end_synth;
-	document.getElementById("id_speak_button").disabled=true;
-	synth.speak(enunt);
+	if(enunt.start())
+	{}
 }
-//-----------------
-
-function on_end_synth()
+//---------
+function on_enunt_end()
 {
+		enunt.stop();
 	
 }
+//------------------
+function on_enunt_results(e)
+{
+	document.getElementById("id_enunt").innerHTML=e.results[0][0].transcript;
+	line(); // functia care traseaza linia
+}
+
+
 //-==================locatia utilizatorului----------
 function on_position_success(e) 
 { 
@@ -32,10 +39,10 @@ function on_position_success(e)
 	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
 	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=600x500"+
 		"&key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
-			"&markers=color:blue|label:F|"+
+			"&markers=color:red|label:F|"+
 				e.coords.latitude  + "," + e.coords.longitude;
 	document.getElementById("id_img").src=map_str;
-	document.getElementById("id_explicatie").innerHTML = "Afisarea locatiei";
+	document.getElementById("id_exp").innerHTML = "Afisarea locatiei";
 	
 }
 //------------------
@@ -44,10 +51,10 @@ function line(e)
 		var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
 	  lat + "," + lon + "&zoom=15"+"&size=600x500"+
 		"key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
-			"&markers=color:blue|label:Z|"+ lat  + "," + lon + 
-	  "&path=color:red|" + lat + "," + lon + "|" + document.getElementById("id_speech").innerHTML;
+			"&markers=color:red|label:F|"+ lat  + "," + lon + 
+	  "&path=color:red|" + lat + "," + lon + "|" + document.getElementById("id_enunt").innerHTML;
 	document.getElementById("id_img").src=map_str;
-	document.getElementById("id_explicatie").innerHTML = "Trasarea liniei de conectare intre pozitia curentare si cea recunoscuta";
+	document.getElementById("id_exp").innerHTML = "Trasarea liniei pozitia curenta si cea prezenta";
 		
 	
 }

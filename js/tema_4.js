984c7444f81f2489c3713b2494ccf3c3e749977f
felyxis <ivancencofelicia@gmail.com>
2018-01-28 23:33:47 +0200

    tema4

--- js/tema_4.js
+++ js/tema_4.js
@@ -1,6 +1,6 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." 
-+ (d.getMonth()+1) + "." + d.getDate() + ".5"; 
++ (d.getMonth()+1) + "." + d.getDate() + ".9"; 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 //------------------------------
 var lat ;
